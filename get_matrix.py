59a1e06ef6da8660515b13b8f1f1bc4807784117
veguinho <rafaelvr@al.insper.edu.br>
2020-11-05 21:05:55 -0300

    parte3

--- get_matrix.py
+++ get_matrix.py
@@ -7,6 +7,9 @@ for i in content:
 colunas = int(float_content[2][0])
 linhas = int(float_content[2][1])
 
+latitude_max_min = float_content[0:1][0]
+longitude_max_min = float_content[1:2][0]
+
 hights = float_content[3:]
 
 
@@ -17,7 +20,7 @@ for l in range(linhas):
         #Inserts vertex (representing the map square unit) for each row * column
         #The y value is set according to the hights vector indicating elevation of coordinate
         position.append(float(c))
-        position.append(hights[l][c]/n)
+        position.append(-hights[l][c]/n)
         position.append(float(l))
 
 indices = []
@@ -37,7 +40,24 @@ for sub in hights:
     for i in sub:
         hights_new.append(i/1200)
 
-print(len(indices))
-# with open('out.txt', 'w') as f:
-#     f.write(str(flat_list_position))
-# f.close()
+f2 = open("SP.ctr", "r")
+s = f2.readlines()
+content2 = [s.strip().split() for s in s] 
+float_content_ctr = []
+for i in content2:
+    float_content_ctr.append([-float(s) for s in i])
+
+line_coords = float_content_ctr[1:]
+
+line_position = []
+
+for coord in line_coords:
+    #Inserts vertex (representing the map square unit) for each row * column
+    #The y value is set according to the hights vector indicating elevation of coordinate
+    y = (-float(coord[1])-latitude_max_min[1])*((linhas-1)/(-latitude_max_min[1]+latitude_max_min[0]))
+    x = (-float(coord[0])-longitude_max_min[1])*((colunas-1)/(-longitude_max_min[1]+longitude_max_min[0]))
+    line_position.append(x)
+    line_position.append(-2-hights[int(y)][int(x)]/n)
+    line_position.append(y)
+
+print(line_position)
